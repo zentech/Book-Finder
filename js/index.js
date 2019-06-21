36de1690d33a9f091d5ec26d91dab6f37c57263b
Jorge Vazquez <jlvazquez825@gmail.com>
2019-06-21 16:01:00 -0400

    added book viewer to app

--- js/index.js
+++ js/index.js
@@ -9,6 +9,7 @@ $(document).ready(function() {
   //listener for search button
   $("#search").click(function() {
     outputList.innerHTML = ""; //empty html output
+    document.body.style.backgroundImage = "url('')";
      searchData = $("#search-box").val();
      //handling empty search input field
      if(searchData === "" || searchData === null) {
@@ -50,6 +51,7 @@ $(document).ready(function() {
         author1 = item.volumeInfo.authors;
         publisher1 = item.volumeInfo.publisher;
         bookLink1 = item.volumeInfo.previewLink;
+        bookIsbn = item.volumeInfo.industryIdentifiers[1].identifier
         bookImg1 = (item.volumeInfo.imageLinks) ? item.volumeInfo.imageLinks.thumbnail : placeHldr ;
 
         item2 = response.items[i+1];
@@ -57,12 +59,13 @@ $(document).ready(function() {
         author2 = item2.volumeInfo.authors;
         publisher2 = item2.volumeInfo.publisher;
         bookLink2 = item2.volumeInfo.previewLink;
+        bookIsbn2 = item2.volumeInfo.industryIdentifiers[1].identifier
         bookImg2 = (item.volumeInfo.imageLinks) ? item.volumeInfo.imageLinks.thumbnail : placeHldr ;
 
         // in production code, item.text should have the HTML entities escaped.
         outputList.innerHTML += '<div class="row mt-4">' +
-                                formatOutput(bookImg1, title1, author1, publisher1, bookLink1) +
-                                formatOutput(bookImg2, title2, author2, publisher2, bookLink2) +
+                                formatOutput(bookImg1, title1, author1, publisher1, bookLink1, bookIsbn) +
+                                formatOutput(bookImg2, title2, author2, publisher2, bookLink2, bookIsbn2) +
                                 '</div>';
 
         console.log(outputList);
@@ -74,8 +77,9 @@ $(document).ready(function() {
    * @param bookImg title author publisher bookLink
    * @return htmlCard
    */
-   function formatOutput(bookImg, title, author, publisher, bookLink) {
+   function formatOutput(bookImg, title, author, publisher, bookLink, bookIsbn) {
      // console.log(title + ""+ author +" "+ publisher +" "+ bookLink+" "+ bookImg)
+     var viewUrl = 'book.html?isbn='+bookIsbn; //constructing link for bookviewer
      var htmlCard = `<div class="col-lg-6">
        <div class="card" style="">
          <div class="row no-gutters">
@@ -87,7 +91,7 @@ $(document).ready(function() {
                <h5 class="card-title">${title}</h5>
                <p class="card-text">Author: ${author}</p>
                <p class="card-text">Publisher: ${publisher}</p>
-               <a target="_blank" href="${bookLink}" class="btn btn-secondary">More Info</a>
+               <a target="_blank" href="${viewUrl}" class="btn btn-secondary">More Info</a>
              </div>
            </div>
          </div>
